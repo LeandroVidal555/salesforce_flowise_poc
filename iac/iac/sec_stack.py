6d347d565ce7e4e644a7b1aab3cda345b89ae4c2
Leandro Vidal <lvidal@humansystems.com>
2024-10-10 12:16:41 -0300

    fix ec2 SG so it allows CF - had to open it to anywhere as it is just an PoC

--- iac/iac/sec_stack.py
+++ iac/iac/sec_stack.py
@@ -103,14 +103,14 @@ class SecurityStack(cdk.Stack):
 
         # Allow traffic from CF - API
         sg_ec2.add_ingress_rule(
-            peer=ec2.Peer.prefix_list(cs['cf_prefix_list']),
+            peer=ec2.Peer.any_ipv4(),
             connection=ec2.Port.tcp(80),
             description="Allow traffic from CF - API"
         )
 
         # Allow traffic from CF - UI
         sg_ec2.add_ingress_rule(
-            peer=ec2.Peer.prefix_list(cs['cf_prefix_list']),
+            peer=ec2.Peer.any_ipv4(),
             connection=ec2.Port.tcp(3000),
             description="Allow traffic from CF - UI"
         )
